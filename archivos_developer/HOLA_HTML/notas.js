4335d56897ed8d1b12c165171529796e46f5eef0
oscardi777 <oscarandresdiazgonz@gmail.com>
2022-12-15 18:06:29 -0500

    mensaje

---
+++ archivos_developer/HOLA_HTML/notas.js
@@ -0,0 +1,7 @@
+//tipos de imagenes
+//lossy lossless
+//sin perdida quiere decir mejor para la pagina, paila para la imagen se la carga
+//con perdida paila para la pagina la hace lenta, chimba pa la imagen se ve bonita
+//lossless gif no perdera calidad y te cobra en la pagina
+//lossless png png 8 256 colores, son imagenes logotipos generalmente con fondo transparente
+//lossless png 24 imagenes mas cercanas tipo fotografia
